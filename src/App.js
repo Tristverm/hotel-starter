7769432079412b8eb3e2b60efc2f39f70cb72419
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-01 21:59:59 +0300

    Added necessarry imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
+// Importing Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+//importing pages
+import Home from "./pages/Home";
+import RoomDetails from "./pages/RoomDetails";
+
+// Importing for React Router
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+function App() {
   return <div>react app</div>;
-};
+}
 
 export default App;
