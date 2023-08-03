e1868592b6cb863a7b58e2752fb43e76de97cc1a
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-03 22:16:52 +0300

    Added context

--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -1,9 +1,19 @@
 import { createContext, useEffect, useState } from "react";
+
 import React from "react";
 
-const RoomContext = () => {
-  console.log("gggg")
-  return <div></div>;
-};
+// importing data
+import { roomData } from "../data";
 
-export default RoomContext;
+//  creating room context
+export const RoomContext = createContext();
+
+function RoomProvider({ children }) {
+  const [rooms, setRooms] = useState(roomData);
+  console.log(rooms);
+  return (
+    <RoomContext.Provider value={{ rooms }}>{children}</RoomContext.Provider>
+  );
+}
+
+export default RoomProvider;
