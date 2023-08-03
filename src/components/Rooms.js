c35f719212cf938db92af44753dfef50f83042b5
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-03 22:37:52 +0300

    Added Emet confif in .vscode

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -3,10 +3,23 @@ import { useContext } from "react";
 //importing Room Context
 import RoomContext from "../context/RoomContext";
 
+// importing rooms
+import Room from "../components/Room"
+
 const Rooms = () => {
   const { rooms } = useContext(RoomContext);
   console.log(rooms);
-  return <div>Rooms</div>;
+  return (
+    <section className="bg-pink-200 py-4">
+      <div className="container mx-auto lg:px-0">
+        <div>
+          {rooms.map((room) => {
+            return <Room />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Rooms;
